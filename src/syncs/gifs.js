a1c2870e0e7cf864de6bd0cb5f61caede267a09b
ivan armolla <ivan.armolla@gmail.com>
2022-11-09 02:15:02 -0300

    adding getBySearch

--- src/syncs/gifs.js
+++ src/syncs/gifs.js
@@ -10,4 +10,13 @@ async function getTrending(updateTrending) {
     });
 }
 
-export default getTrending;
+async function getBySearch(query) {
+  const request = axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}?&${apiKey}`).then((result) => {
+    console.log(result.data)
+  })
+}
+
+export default {
+  getTrending,
+  getBySearch
+};
