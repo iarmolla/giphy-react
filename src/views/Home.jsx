cf09db6ff0e4979216fe6cb45e67f5160f5a1d3f
iarmolla <ivan.armolla@gmail.com>
2022-11-09 02:51:37 -0300

    adding props navbar

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,24 +5,21 @@ import NavBar from '../components/NavBar'
 
 function Home() {
 
-    const [trending, updateTrending] = useState()
+    const [gifs, updateGifs] = useState()
     useEffect(() => {
-        // getTrending(updateTrending)
-        // getBySearch('chainsaw man')
-        get.getBySearch('chainsaw man')
+        syncs.getTrending(updateGifs)
     }, [])
-    console.log(trending)
     return (
         <div>
             <div className='flex justify-center'>
-            <NavBar></NavBar>
+                <NavBar updateGifs={updateGifs}></NavBar>
             </div>
             <div className='container-gif '>
                 {
-                    trending?.data.map((gif) => {
+                    gifs?.data.map((gif) => {
                         return (
                             <div key={gif.id} className='gif-card'>
-                                
+
                                 <div className='gif-link'>
                                     <img src={gif.images.original.url} alt="" />
                                     <h3 className='gif-title'>{gif.title}</h3>
