961d44799b7ab9880c04c4bbc33ceb011030e1b9
iarmolla <ivan.armolla@gmail.com>
2022-11-15 00:11:40 -0300

    adding styles to image-icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ function Modal({ image, showModal, setShowModal }) {
                     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center hover:opacity-100">
                         <div className="relative rounded m-5">
                             <img src={image} alt="" className='modal-image ' />
-                            <div className='absolute top-0 p-2 rounded-lg text-sm hover:text-white cursor-pointer' onClick={()=> setShowModal(false)}>
+                            <div className='absolute top-0 p-2 rounded-lg text-sm image-icon hover:text-white cursor-pointer' onClick={()=> setShowModal(false)}>
                                 <box-icon name='x' color='#fffafa'></box-icon>
                             </div>
                         </div>
