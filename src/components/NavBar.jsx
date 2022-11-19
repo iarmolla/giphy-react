49c56f9fb488f4ee6b94d48485625c70844931ad
iarmolla <ivan.armolla@gmail.com>
2022-11-19 02:36:19 -0300

    adding redirect to login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,22 @@
 import syncs from "../syncs/gifs";
+import { Link } from "react-router-dom";
 
 function NavBar({ ...props }) {
   return (
     <>
       <nav className="border-gray-200 sm:gap-0 sm:p-5 m-5 rounded lg:w-3/6 ml-3 sm:w-3/4">
         <div className="container flex flex-wrap justify-around items-center mx-auto">
-          <a href="https://flowbite.com/" className="flex items-center">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/fr/a/a0/Giphy_logo.gif"
-              className="mr-3 h-6 sm:h-9 "
-              alt="Flowbite Logo"
-            />
-          </a>
-          <div className="flex gap-7 items-center">            
+          <img
+            src="https://upload.wikimedia.org/wikipedia/fr/a/a0/Giphy_logo.gif"
+            className="mr-3 h-6 sm:h-9 "
+            alt="Logo"
+          />
+          <div className="flex gap-7 items-center">
             <div className="flex flex-row items-center w-max">
               <div className="w-max flex items-center bg-zinc-600 rounded-l-sm p-2">
                 <box-icon name='user' type='solid' color='#fffefe'></box-icon>
-              </div>              
-              <button className="bg-zinc-700 rounded-r-sm px-5 sm:px-5 lg:px-20 py-2">Login</button>
+              </div>
+              <Link to={'/login'} className="bg-zinc-700 rounded-r-sm px-5 sm:px-5 lg:px-20 py-2">Login</Link>
             </div>
           </div>
         </div>
