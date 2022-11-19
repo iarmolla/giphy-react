b47596e3e9947fc7da8f57ca7e62488aa175c024
iarmolla <ivan.armolla@gmail.com>
2022-11-19 02:32:43 -0300

    adding signup

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,12 +5,11 @@ import { motion } from "framer-motion"
 function Login() {
   const spring = {
     type: "spring",
-    stiffness: 700,
+    stiffness: 200,
     damping: 30
   };
   const [isOn, setIsOn] = useState(false);
   const toggleSwitch = () => setIsOn(!isOn);
-  console.log(isOn)
   return (
     <div className='sm:h-screen grid sm:grid-cols-2 login'>
       <form action="POST" className='flex flex-col justify-center items-center'>
@@ -27,28 +26,38 @@ function Login() {
           <input
             className="outline-0 text-left text-gray-900 text-sm rounded-sm py-3 w-2/4 pl-5"
             placeholder="Email address" />
+          {
+            isOn ? <>
+              <input
+                className="outline-0 text-left text-gray-900 text-sm rounded-sm py-3 w-2/4 pl-5"
+                placeholder="Username" type='text' />
+              <input
+                className="outline-0 text-left text-gray-900 text-sm rounded-sm py-3 w-2/4 pl-5"
+                placeholder="Confirm your Password" type='password' />
+            </> : <></>
+          }
           <input
             className="outline-0 text-left text-gray-900 text-sm rounded-sm py-3 w-2/4 pl-5"
             placeholder="Password" type='password' />
-          <button className="outline-0 text-center bg-slate-200 text-sm rounded-sm py-3 w-2/4 pl-3 login-submit">Login</button>
+          <button className="outline-0 text-center bg-slate-200 text-sm rounded-sm py-3 w-2/4 pl-3 login-submit">{!isOn ? 'Log in' : 'Sign up'}</button>
         </div>
         <div className='mt-4'>
-          <span className='text-cyan-500 text-xs font-bold'>Forgot Your Password?</span>
+          <span className='text-cyan-500 text-xs font-bold'>{!isOn ? 'Forgot Your Password?' : ''}</span>
         </div>
         <div className='flex flex-col w-full items-center justify-center gap-3 mt-4'>
-          <button className='form-logs relative  rounded-sm text-center py-3 w-2/4'>Log in with Facebook
+          <button className='form-logs relative  rounded-sm text-center py-3 w-2/4'>{ !isOn ? 'Log in with Facebook' : 'Sign up with facebook'}
             <div className='absolute top-3 left-20 sm:left-3 sm:top-3'>
               <img className='h-7 rounded-md hidden sm:hidden md:hidden lg:block' src="https://www.pngkey.com/png/full/509-5094426_facebook-twitter-instagram-pinterest-facebook-logo-in-circle.png" alt="" />
             </div>
           </button>
-          <button className='form-logs relative rounded-sm text-center py-3 w-2/4'>Log in with Apple
+          <button className='form-logs relative rounded-sm text-center py-3 w-2/4'>{!isOn ? 'Log in with Apple' : 'Sign up in with Apple'}
             <div className='absolute top-3 left-20 sm:left-3 '>
               <img className='h-7 rounded-md hidden sm:hidden md:hidden lg:block' src='https://www.transparentpng.com/thumb/apple-logo/UyOL60-apple-logo-wonderful-picture-images.png' alt="" />
             </div>
           </button>
         </div>
         <div className='mt-1'>
-          <span className='text-xs font-bold'>By logging in you agree to GIPHY's<span className='text-cyan-500 text-xs font-bold'>Terms of Service</span></span>
+          <span className='text-xs font-bold'>By logging in you agree to GIPHY's <span className='text-cyan-500 text-xs font-bold'>Terms of Service</span></span>
         </div>
       </form>
       <div className='bg-[url(https://64.media.tumblr.com/e05172187a870c27dce5e119178dab32/905eb58846d763f4-4c/s540x810/5e2f930c3665bfebf17c10834bf6cc15c91ae9c1.gifv)]'>
